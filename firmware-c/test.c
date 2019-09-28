cea0d0f823786cde1c11ceb2309ac5431fa778c6
obijuan <juan@iearobotics.com>
2019-09-28 21:48:33 +0200

    test.c updated (removed old firmaware*.c)

---
+++ firmware-c/test.c
@@ -0,0 +1,102 @@
+
+#include <stdint.h>
+
+//-- Registros mapeados
+#define reg_uart_data (*(volatile uint32_t*)0x02000008)
+#define reg_leds (*(volatile uint32_t*)0x03000000)
+
+// --------------------------------------------------------
+
+void putchar(char c)
+{
+	if (c == '\n')
+		putchar('\r');
+	reg_uart_data = c;
+}
+
+void print(const char *p)
+{
+	while (*p)
+		putchar(*(p++));
+}
+
+char getchar_prompt(char *prompt)
+{
+	int32_t c = -1;
+	int32_t count = 0;
+
+	uint32_t cycles_begin, cycles_now, cycles;
+	__asm__ volatile ("rdcycle %0" : "=r"(cycles_begin));
+
+	reg_leds = ~0;
+	count = 0;
+
+	if (prompt)
+		print(prompt);
+
+	while (c == -1) {
+		__asm__ volatile ("rdcycle %0" : "=r"(cycles_now));
+		cycles = cycles_now - cycles_begin;
+		if (cycles > 2000000) {
+			if (prompt)
+				print(prompt);
+			cycles_begin = cycles_now;
+			count += 1;
+			reg_leds = count;
+		}
+		c = reg_uart_data;
+	}
+
+	reg_leds = 0;
+	return c;
+}
+
+char getchar()
+{
+	return getchar_prompt(0);
+}
+
+void menu()
+{
+	print("\n");
+	print("  ____  _          ____         ____\n");
+	print(" |  _ \\(_) ___ ___/ ___|  ___  / ___|\n");
+	print(" | |_) | |/ __/ _ \\___ \\ / _ \\| |\n");
+	print(" |  __/| | (_| (_) |__) | (_) | |___\n");
+	print(" |_|   |_|\\___\\___/____/ \\___/ \\____|\n");
+	print("\nRunning on the Alhambra II board :-)\n");
+	print("\n");
+}
+
+// --------------------------------------------------------
+
+void main()
+{
+	reg_leds = 0x1F;
+	print("Booting...\n\n ");
+
+	reg_leds = 0x7F;
+	print("Press ENTER to continue...");
+	while (getchar_prompt(0) != '\r') { /* wait */ }
+
+	menu();
+
+	while (1)
+	{
+			print("Command> ");
+			char cmd = getchar();
+			if (cmd > 32 && cmd < 127)
+				putchar(cmd);
+			print("\n");
+
+			switch (cmd)
+			{
+			case '1':
+			  menu();
+				break;
+			default:
+				continue;
+			}
+
+	}
+}
