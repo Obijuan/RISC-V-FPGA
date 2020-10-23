79d743fee4dbab327a5041a9e5f521220346870d
Obijuan <juan@iearobotics.com>
2020-10-23 10:29:20 +0200

    mmio-1: counter and bash scripts for rars

---
+++ firmware/soc-rars-MMIO-1-port/rars/counter.sh
@@ -0,0 +1,3 @@
+java -jar rars1_5.jar a dump .text Binary counter.bin counter.s  
+apio raw "iceprog -o 1M counter.bin"
+
---
+++ firmware/soc-rars-MMIO-1-port/rars/leds-on.sh
@@ -0,0 +1,3 @@
+java -jar rars1_5.jar a dump .text Binary leds-on.bin leds-on.s  
+apio raw "iceprog -o 1M leds-on.bin"
+
